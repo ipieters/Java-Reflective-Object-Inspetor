d82197ffb5513f01f79f7d2917d8e0da665124de
ipieters <ipiet870@mtroyal.ca>
2019-10-25 21:15:40 -0600

    Added tests and fixed some printing

--- src/Inspector.java
+++ src/Inspector.java
@@ -8,145 +8,146 @@ import java.lang.reflect.Modifier;
 
 public class Inspector {
 
-    public void inspect(Object obj, boolean recursive) {
-        Class c = obj.getClass();
-        inspectClass(c, obj, recursive, 0);
-    }
+	public void inspect(Object obj, boolean recursive) {
+		Class c = obj.getClass();
+		inspectClass(c, obj, recursive, 0);
+	}
 
-    private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
-    	printHelper("Class Name ["+ c.getName() +"]",0);
-    	
-    	inspectSuperClass(c, obj, recursive, depth);
-    	inspectInterface(c, obj, recursive, depth);
-    	inspectConstructor(c, obj, depth);
-    	
-    }
-    
-    public void inspectSuperClass(Class c, Object obj, boolean recursive, int depth) {
+	public void inspectClass(Class c, Object obj, boolean recursive, int depth) {
+		inspectClassName(c);
+		inspectSuperClass(c, obj, recursive, depth);
+		inspectInterface(c, obj, recursive, depth);
+		inspectConstructor(c, obj, depth);
+		inspectMethods(c, depth);
+		inspectFields(c, obj, recursive, depth);
+	}
 
-        if (c.equals(Object.class))
-            return;
-        
-        Class superClass = c.getSuperclass();
+	public void inspectClassName(Class c) {
+		printHelper("Class Name [" + c.getName() + "]", 0);
+	}
+	public void inspectSuperClass(Class c, Object obj, boolean recursive, int depth) {
 
-        if (superClass != null) {
-        	printHelper("SuperClass [" + superClass.getName() +"]", depth);
-            inspectClass(superClass, obj, recursive, depth+1);
-        } else
-            printHelper("There is no super class", depth);
-        
-    }
-    
+		if (c.equals(Object.class))
+			return;
 
-    public void inspectInterface(Class c, Object obj, boolean recursive, int depth) {
+		Class superClass = c.getSuperclass();
 
-        Class[] interfaces = c.getInterfaces();
+		if (superClass != null) {
+			printHelper("SuperClass [" + superClass.getName() + "]", depth);
+			inspectClass(superClass, obj, recursive, depth + 1);
+		} else
+			printHelper("There is no super class", depth);
 
-        for (Class inter : interfaces) {
-            printHelper("Interface [" + inter.getName() + "]", depth);
-            inspectClass(inter, obj, recursive, depth+1);
-        }
-    }
+	}
 
-    public void inspectConstructor(Class c, Object obj, int depth) {
+	public void inspectInterface(Class c, Object obj, boolean recursive, int depth) {
 
-        Constructor[] constructors = c.getDeclaredConstructors();
+		Class[] interfaces = c.getInterfaces();
 
-        for (Constructor constructor: constructors) {
-            printHelper("Constructor [" + constructor.getName()+"]", depth);
+		for (Class inter : interfaces) {
+			printHelper("Interface [" + inter.getName() + "]", depth);
+			inspectClass(inter, obj, recursive, depth + 1);
+		}
+	}
 
-            Class[] parameters = constructor.getParameterTypes();
-            for (Class parameter: parameters) {
-                printHelper("Parameters [" + parameter.getName() +"]", depth+1);
-            }
+	public void inspectConstructor(Class c, Object obj, int depth) {
 
-            printHelper("Modifiers [" + Modifier.toString(constructor.getModifiers()) + "]", depth+1);
-        }
-    }
+		Constructor[] constructors = c.getDeclaredConstructors();
 
-    public void inspectMethods(Class c, int depth) {
+		for (Constructor constructor : constructors) {
+			printHelper("Constructor [" + constructor.getName() + "]", depth);
 
-        Method[] methods = c.getDeclaredMethods();
+			Class[] parameters = constructor.getParameterTypes();
+			for (Class parameter : parameters)
+				printHelper("Parameter [" + parameter.getName() + "]", depth + 1);
 
-        for (Method method: methods) {
-            printHelper("Method Name [" + method.getName() + "]", depth);
+			printHelper("Modifier [" + Modifier.toString(constructor.getModifiers()) + "]", depth + 1);
+		}
+	}
 
-            Class[] exceptions = method.getExceptionTypes();
-            for (Class exception: exceptions) {
-                printHelper("Exceptions [" + exception.getName() + "]", depth+1);
-            }
+	public void inspectMethods(Class c, int depth) {
 
-            Class[] parameters = method.getParameterTypes();
-            for (Class parameter: parameters) {
-                printHelper("Parameters [" + parameter.getName() + "]", depth+1);
-            }
+		Method[] methods = c.getDeclaredMethods();
 
-            printHelper("Returns [" + method.getReturnType().getName() + "]", depth+1);
-            printHelper("Modifiers [" + Modifier.toString(method.getModifiers()) + "] q", depth+1);
-        }
-    }
-    
-    public void inspectFields(Class c, Object obj, boolean recursive, int depth) {
+		for (Method method : methods) {
+			printHelper("Method [" + method.getName() + "]", depth);
 
-        Field[] fields = c.getDeclaredFields();
+			Class[] exceptions = method.getExceptionTypes();
+			for (Class exception : exceptions)
+				printHelper("Exceptions [" + exception.getName() + "]", depth + 1);
 
-        for (Field field: fields) {
-            printHelper("Field Name [" + field.getName() + "]", depth);
-            printHelper("Type [" + field.getType().getName() + "]", depth+1);
-            printHelper("Modifiers [" + Modifier.toString(field.getModifiers()) + "]", depth+1);
+			Class[] parameters = method.getParameterTypes();
+			for (Class parameter : parameters)
+				printHelper("Parameter [" + parameter.getName() + "]", depth + 1);
 
-            field.setAccessible(true);
+			printHelper("Return [" + method.getReturnType().getName() + "]", depth + 1);
+			printHelper("Modifier [" + Modifier.toString(method.getModifiers()) + "]", depth + 1);
+		}
+	}
 
-            Object value = null;
-            try {
-                value = field.get(obj);
-            } catch (IllegalAccessException e) {
-                System.out.println("Unable to access field");
-            }
+	public void inspectFields(Class c, Object obj, boolean recursive, int depth) {
 
-            if (field.getType().isArray()) {
-                inspectArray(field.getType(), value, recursive, depth);
-            } else if (field.getType().isPrimitive()) {
-                printHelper("Value [" + value + "]", depth+1);
-            } else {
-                inspectReferenceValue(recursive, depth, value);
-            }
-        }
-    }
-    
-    public void inspectArray(Class c, Object obj, boolean recursive, int depth) {
+		Field[] fields = c.getDeclaredFields();
 
-        Class componentType = c.getComponentType();
-        printHelper("Component Type [" + componentType.getName() + "]", depth);
-        printHelper("Array Length [" + Array.getLength(obj) + "]", depth);
+		for (Field field : fields) {
+			printHelper("Field Name [" + field.getName() + "]", depth);
+			printHelper("Type [" + field.getType().getName() + "]", depth + 1);
+			printHelper("Modifier [" + Modifier.toString(field.getModifiers()) + "]", depth + 1);
 
-        for (int i = 0; i < Array.getLength(obj); i++) {
-            Object object = Array.get(obj, i);
+			field.setAccessible(true);
 
-            if (object == null) {
-                printHelper("null", depth);
-            } else if (componentType.isPrimitive()) {
-                printHelper(object.getClass().getName(), depth);
-            } else if (componentType.isArray()) {
-                inspectArray(object.getClass(), object, recursive, 1);
-            } else {
-                inspectReferenceValue(recursive, depth, object);
-            }
-        }
-        System.out.println();
-    }
+			Object value = null;
+			try {
+				value = field.get(obj);
+			} catch (IllegalAccessException e) {
+				System.out.println("Unable to access field");
+			}
 
-    private void inspectReferenceValue(boolean recursive, int depth, Object object) {
-        if (recursive)
-            inspectClass(object.getClass(), object, recursive, depth+1);
-       else
-            printHelper("Reference Value [" + object.getClass().getName() + "@" + object.hashCode() + "]", depth);
-        
-    }
-    public void printHelper(String message, int indent) {
-        for (int i = 0; i < indent; i++) {
-            System.out.print("\t");
-        }
-        System.out.println(message);
-    }
+			if (value == null)
+				printHelper("Value [" + null + "]", depth + 1);
+			else if (field.getType().isArray())
+				inspectArray(field.getType(), value, recursive, depth);
+			else if (field.getType().isPrimitive())
+				printHelper("Value [" + value + "]", depth + 1);
+			else
+				inspectReferenceValue(recursive, depth, value);
+		}
+	}
+
+	public void inspectArray(Class c, Object obj, boolean recursive, int depth) {
+
+		Class componentType = c.getComponentType();
+		printHelper("Component Type [" + componentType.getName() + "]", depth);
+		printHelper("Array Length [" + Array.getLength(obj) + "]", depth);
+
+		for (int i = 0; i < Array.getLength(obj); i++) {
+			Object object = Array.get(obj, i);
+
+			if (object == null)
+				printHelper("Value [" + i + "] = null", depth + 1);
+			else if (componentType.isPrimitive())
+				printHelper("Value [" + i + "] = " + object.getClass().getName(), depth + 1);
+			else if (componentType.isArray())
+				inspectArray(object.getClass(), object, recursive, 1);
+			else
+				inspectReferenceValue(recursive, depth, object);
+
+		}
+		System.out.println();
+	}
+
+	private void inspectReferenceValue(boolean recursive, int depth, Object object) {
+		if (recursive)
+			inspectClass(object.getClass(), object, recursive, depth + 1);
+		else
+			printHelper("Reference Value [" + object.getClass().getName() + "@" + object.hashCode() + "]", depth);
+
+	}
+
+	public void printHelper(String message, int indent) {
+		for (int i = 0; i < indent; i++)
+			System.out.print("\t");
+
+		System.out.println(message);
+	}
 }
---
+++ src/TestInspector.java
@@ -0,0 +1,94 @@
+package src;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.Serializable;
+
+public class TestInspector {
+
+    private static final ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outStream));
+    }
+
+    @After
+    public void restoreStreams() {
+        outStream.reset();
+    }
+
+    @Test
+    public void testHasSuperClass() {
+        new Inspector().inspectSuperClass(String.class, "", false, 0 );
+        String expected = "SuperClass [java.lang.Object]";
+        assert(outStream.toString().contains(expected));
+    }
+    
+    @Test
+    public void testHasInterface() {
+        new Inspector().inspectInterface(String.class, ""  ,false, 0);
+        String expected = "Interface [java.io.Serializable]";
+        assert(outStream.toString().contains(expected));
+    }
+    
+    @Test
+    public void testConstructor() {
+        new Inspector().inspectConstructor(String.class, "", 0);
+        String expected = "Constructor [java.lang.String]";
+        assert (outStream.toString().contains(expected));
+
+        expected = "Parameter [int]";
+        assert (outStream.toString().contains(expected));
+
+        expected = "Modifier [public]";
+        assert (outStream.toString().contains(expected));
+    }
+    
+    @Test
+    public void testMethods() {
+        new Inspector().inspectMethods(String.class, 0);
+
+        String expected = "Method [length]";
+        assert (outStream.toString().contains(expected));
+
+        expected = "Exceptions [java.io.UnsupportedEncodingException]";
+        assert (outStream.toString().contains(expected));
+
+        expected = "Parameter [int]";
+        assert (outStream.toString().contains(expected));
+        expected = "Return [int]";
+        assert (outStream.toString().contains(expected));
+
+        expected = "Modifier [public]";
+        assert (outStream.toString().contains(expected));
+
+    }
+    
+    @Test
+    public void testFields() {
+        new Inspector().inspectFields(String.class, "", false, 0);
+
+        String expected = "Field Name [hash]";
+        assert (outStream.toString().contains(expected));
+
+        expected = "Type [int]";
+        assert (outStream.toString().contains(expected));
+
+        expected = "Modifier [private]";
+        assert (outStream.toString().contains(expected));
+
+        expected = "Value [0]";
+        assert (outStream.toString().contains(expected));
+    }
+
+    
+    
+
+
+  
+}
